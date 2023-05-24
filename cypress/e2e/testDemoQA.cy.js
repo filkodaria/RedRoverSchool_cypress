e3fe611cfa2da65c4f42ac0a2432d3045685ebc6
filkodaria <filko_d@ukr.net>
2023-05-24 23:59:26 +0300

    testDemoQA updated

--- cypress/e2e/testDemoQA.cy.js
+++ cypress/e2e/testDemoQA.cy.js
@@ -1,24 +1,33 @@
-describe('tiles Elements', () => {
-	
+describe('Menu', () => {
 	beforeEach(() => {
-		cy.visit('https://demoqa.com');
-	});
-	
-	it.skip('clicking the tile Elements', () => {
-		cy.get("div.card-body h5").contains("Elements").click();
-		cy.url().should("include", "/elements");
-		cy.get("div.main-header").should('have.text', 'Elements');
+		cy.visit("https://demoqa.com");
 	});
 
-	it.skip("clicking the dropdown Text Box", () => {
+	it('count tiles', () => {
+		cy.get("div.card").should('have.length', 6);
+	});
+})
+
+
+describe("Tile Elements", () => {
+  beforeEach(() => {
+    cy.visit("https://demoqa.com");
+  });
+
+  it.skip("clicking the tile Elements", () => {
+    cy.get("div.card-body h5").contains("Elements").click();
+    cy.url().should("include", "/elements");
+    cy.get("div.main-header").should("have.text", "Elements");
+  });
+
+  it.skip("clicking the dropdown Text Box", () => {
     cy.get("div.card-body h5").contains("Elements").click();
     cy.get("#item-0 span").contains("Text Box").click();
-	});
+  });
 
-	it('fill out the field Full Name', () => {
-		cy.get("div.card-body h5").contains("Elements").click();
-		cy.get("#item-0 span").contains("Text Box").click();
-		cy.get('#userName').type('Daria');
-	});
-	
-})
+  it.skip("fill out the field Full Name", () => {
+    cy.get("div.card-body h5").contains("Elements").click();
+    cy.get("#item-0 span").contains("Text Box").click();
+    cy.get("#userName").type("Daria");
+  });
+});
