d8ce84893a5533167b5a4d4b13307023c4aafc8b
filkodaria <filko_d@ukr.net>
2023-06-04 23:36:36 +0300

    testDemoQA updated

--- cypress/e2e/testDemoQA.cy.js
+++ cypress/e2e/testDemoQA.cy.js
@@ -1,30 +1,58 @@
-describe('Menu', () => {
+// HOME PAGE
+describe.skip("Menu", () => {
   const expectedMenuItemNames = [
     "Elements",
     "Forms",
     "Alerts, Frame & Windows",
     "Widgets",
     "Interactions",
-    "Book Store Application"
+    "Book Store Application",
   ];
-  
+
   beforeEach(() => {
     cy.visit("https://demoqa.com");
   });
 
-  it('count tiles', () => {
-    cy.get("div.card").should('have.length', expectedMenuItemNames.length);
+  it("count tiles", () => {
+    cy.get("div.card").should("have.length", expectedMenuItemNames.length);
   });
-  
-  it("verify menu item names", function () {
+
+  it("verify menu item names - var#1", function () {
     cy.get(".card").each(($el, idx) => {
       cy.log($el.text());
       expect($el.text()).to.be.equal(expectedMenuItemNames[idx]);
     });
   });
+
+  it("verify menu item names - var#2", function () {
+    cy.get(".card")
+      .then(($els) => {
+        // cy.log(Cypress.$.makeArray($els).map($el => $el.innerText));
+        return Cypress.$.makeArray($els).map(($el) => $el.innerText);
+      })
+      .should('deep.equal', expectedMenuItemNames)
+  });
+
+  it("verify menu item names - var#3", function () {
+    cy.get(".card")
+      .then(($els) => {
+        let actualArray = Cypress.$.makeArray($els).map(($el) => $el.innerText);
+        expect(actualArray).to.be.deep.equal(expectedMenuItemNames);
+      });
+  });
+
+  it("verify menu item names - var#4", function () {
+    cy.get(".card")
+      .then(($els) => {
+        return Cypress._.map($els, "innerText");
+      })
+      .should("deep.equal", expectedMenuItemNames);
+  });
+
 });
 
 
+// TILE ELEMETS
 describe.skip("Tile Elements", () => {
   beforeEach(() => {
     cy.visit("https://demoqa.com");
@@ -46,4 +74,37 @@ describe.skip("Tile Elements", () => {
     cy.get("#item-0 span").contains("Text Box").click();
     cy.get("#userName").type("Daria");
   });
+
+});
+
+
+// TILE FORMS
+describe("Tile Forms", () => {
+  beforeEach(() => {
+    cy.visit("https://demoqa.com");
+  });
+
+  it("clicking the tile Forms", () => {
+    cy.get("div.card-body h5").contains("Forms").click();
+    cy.url().should("include", "/forms");
+    cy.get("div.main-header").should("have.text", "Forms");
+  });
+
+  it("clicking the dropdown Practice Form", () => {
+    cy.get("div.card-body h5").contains("Forms").click();
+    cy.get("#item-0 span").contains("Practice Form").click();
+  });
+
+  it("fill out the section Name", () => {
+    cy.get("div.card-body h5").contains("Forms").click();
+    cy.get("#item-0 span").contains("Practice Form").click();
+    cy.get("#firstName").type("Daria");
+    cy.get("#lastName").type("Filko");
+    cy.get("#userEmail").type("testdf@df.mail");
+    cy.get("[for='gender-radio-2']").click();
+    cy.get("#userNumber").type("0123456789");
+    cy.get("#dateOfBirth").click();
+
+  });
+  
 });
